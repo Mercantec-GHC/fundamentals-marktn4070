ab220af16f8b9377f76c7a8018e825d4d10628d1
Mark <snapstil1eren@gmail.com>
2024-10-15 07:44:41 +0200

    test

--- Fundamentiels/Opg_dag_1/Program.cs
+++ Fundamentiels/Opg_dag_1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, dag 2!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
